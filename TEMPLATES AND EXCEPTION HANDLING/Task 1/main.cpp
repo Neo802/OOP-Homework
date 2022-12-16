b4836af0b22e98ee7633d3589cc9dc93ddc30726
Albert <gamescellrblx@protonmail.com>
2022-12-16 22:57:05 +0200

    Update to main.cpp
    Task 1 for TEH, finished

--- TEMPLATES AND EXCEPTION HANDLING/Task 1/main.cpp
+++ TEMPLATES AND EXCEPTION HANDLING/Task 1/main.cpp
@@ -1,6 +1,21 @@
+// TEH - Task 1
 #include <iostream>
+using namespace std;
+
+template <typename T> T Sum(T x, T y) {
+    cout << typeid(T).name() << " Sum\n";
+    return x + y;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float a, b;
+    double c, d;
+
+    cin >> a >> b >> c >> d;
+
+    cout << endl;
+    cout << Sum(a, b);
+    cout << endl;
+    cout << Sum(c, d);
 }
